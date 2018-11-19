cedba4c6c20a80414c66a91324d98ae5929a63a7
mrtan <freemrtan@gmail.com>
2018-11-19 09:40:32 +0800

    add sample code

--- app/src/test/java/com/mrtan/test/CoroutinesTest.kt
+++ app/src/test/java/com/mrtan/test/CoroutinesTest.kt
@@ -1,17 +1,53 @@
 package com.mrtan.test
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
+import kotlin.coroutines.CoroutineContext
 
-fun main() = runBlocking{
-  withTimeout(1300){
-    repeat(1000){
-      println("I'm sleeping $it")
-      delay(500)
+fun main() = {
+  val job = Job()
+  runBlocking(job){
+    val job = CompletableDeferred<Any>()
+    job.invokeOnCompletion {
+      println(job.isCancelled)
+      println("finish")
+    }
+    job.cancel()
+  }
+
+
+
+}
+// api service
+fun api(): Deferred<String> = CompletableDeferred()
+
+class Activity(): CoroutineScope {
+  val job = Job()
+  override val coroutineContext: CoroutineContext = Dispatchers.Default + job
+
+  fun runApi(){
+    launch {
+      val deferred = api()
+      job.invokeOnCompletion {
+        if (job.isCancelled)
+          deferred.cancel()
+      }
+      try{
+        val result = deferred.await()
+        //todo
+      } catch (t: Throwable){
+        // exception control
+      }
     }
   }
+
+  fun destory(){
+    job.cancel()
+  }
+
 }
